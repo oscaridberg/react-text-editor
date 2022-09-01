8b68111580f2e29ee83d9e11ce08b109f5de1d4d
oscaridberg <idberg.oscar@gmail.com>
2022-09-01 14:54:47 +0200

    add tinymce text editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from 'react';
+import TextEditor from './components/TextEditor.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>hello_world</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextEditor/>
     </div>
   );
 }
