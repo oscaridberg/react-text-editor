ec25e9a24a31219c268ace9baa6b10c945108298
oscaridberg <idberg.oscar@gmail.com>
2022-09-14 15:39:57 +0200

    fix save-bug with new document

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,31 +1,83 @@
 import { Editor } from '@tinymce/tinymce-react';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import config from '../config/config.json';
-// import ToolBar from './ToolBar';
+import docsModel from '../models/docs';
 import { useState } from 'react';
 
 
+
 export default function TextEditor() {
+    const [docs, setDocs] = useState([]);
+    const [currentDoc, setCurrentDoc] = useState([]);
+
     const editorRef = useRef(null);
+    
     const log = () => {
       if (editorRef.current) {
-        console.log(editorRef.current.getContent());
+        const title = document.getElementById('documentTitle').value;
+        const content = editorRef.current.getContent();
+        const request = {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({ title: title, content: content })
+        };
+
+        fetch(`${config.base_url}/documents`, request)
+          .then(response => response.json());
       }
     };
 
+    useEffect(() => {
+      (async () => {
+          const allDocs = await docsModel.getAllDocs();
+          setDocs(allDocs);
+      })();
+    }, []);
+
+
+    // useEffect((event) => {
+    //   (async () => {
+    //       const title = event.target.value;
+
+    //       const currentDoc = await docsModel.getOneDoc(title);
+
+    //       setCurrentDoc(currentDoc);
+    //       editorRef.current.setContent(currentDoc.content);
+
+    //   })();
+    // }, []);
+
+    async function fetchDoc(event) {
+        const title = event.target.value;
+        const currentDoc = await docsModel.getOneDoc(title);
+        setCurrentDoc(currentDoc);
+    };
+
+ 
 
     return (
       <div className="editorContainer">
         <h1 className='appTitle'>React Text Editor</h1>
         <div className='toolBar'>
           <button className='saveButton' onClick={log}>Save</button>
+          <select
+          onChange={fetchDoc}
+          >
+          <option id='docChoice' value="-99" key="0">Choose a document</option>
+          {docs.map((doc, index) => <option value={doc.title} key={index}>{doc.title}</option>)}
+          </select>
         </div>
+
+      <form className='documentTitle'>
+        <label for="documentTitle">Document Title</label>
+        <input type="text" id="documentTitle" className='titleField' defaultValue={currentDoc.title}></input>
+      </form>
 
         <div className='textEditor'>
           <Editor
             apiKey={config.api_key}
             onInit={(evt, editor) => editorRef.current = editor}
-            initialValue="<p>This is the initial content of the editor.</p>"
+            initialValue={currentDoc.content}
             init={{
               height: 500,
               menubar: false,
@@ -46,3 +98,4 @@ export default function TextEditor() {
       </div>
     );
   }
+
---
+++ src/models/docs.js
@@ -0,0 +1,24 @@
+import config from '../config/config.json';
+
+
+const docs = {
+    getAllDocs: async function getAllDocs () {
+        const response = await fetch(`${config.base_url}/documents`);
+        const result = await response.json();
+        return result;
+    },
+
+    getOneDoc: async function getOneDoc (title) {
+        const response = await fetch(`${config.base_url}/documents`);
+        const result = await response.json();
+        let currentDoc = "";
+        const foundDoc = result.forEach(function (arrayitem) {
+            if (arrayitem.title === title) {
+                currentDoc =  arrayitem;
+            }
+        })
+        return currentDoc;
+    }
+}
+
+export default docs;
