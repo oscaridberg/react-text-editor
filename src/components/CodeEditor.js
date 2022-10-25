a2dff4083b346e5c29153743022cf0d0721cd458
Oscar Idberg <oscaridberg@pop-os.localdomain>
2022-10-25 05:47:26 +0200

    Add terminal

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -8,12 +8,38 @@ import MonacoEditor from 'react-monaco-editor';
 import Editor from "@monaco-editor/react";
 
 export default function CodeEditor({valueGetter, isEditorReady, setIsEditorReady, editorRef, code, setCode, currentDoc, setCurrentDoc, docs, fetchDoc, saveDoc, setPopup}) {
+    const [terminal, setTerminal] = useState('');
 
 
     function handleEditorDidMount(_valueGetter) {
         setIsEditorReady(true);
         valueGetter.current = _valueGetter;
     }
+
+    async function runCode() {
+        const code = valueGetter.current.getValue();
+
+        var data = {
+            code: btoa(code)
+        };
+
+        const response = await fetch("https://execjs.emilfolino.se/code", {
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'},
+            method: 'POST'
+        });
+
+        const result = await response.json();
+
+        let decodedOutput = atob(result.data);
+
+        setTerminal(decodedOutput);
+        // return result;
+    }
+
+    const changeHandler = (evt, newText) => {
+        setCode(newText);
+      };
 
     return (
     <div className="editorContainer">
@@ -34,9 +60,17 @@ export default function CodeEditor({valueGetter, isEditorReady, setIsEditorReady
         language="javascript"
         theme="vs-dark"
         value={currentDoc['content']}
+        
 
 
       />
+    </div>
+
+    <div className="terminalContainer">
+        <div className="terminalWindow">
+            <p className="terminalText">terminal~ {terminal}</p>
+        </div>
+        <button className='terminalButton' onClick={runCode}>Run code</button>
     </div>    
 
 
